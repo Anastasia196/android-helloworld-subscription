92ef6803af17b8903b364f05cb3662b3f9b2e970
Farkhad Rakhimzhanov <farkhad@gmail.com>
2019-08-15 16:52:46 +0600

    Added layout and logic of Subscription form

--- app/src/main/java/com/example/androidsubscription/MainActivity.java
+++ app/src/main/java/com/example/androidsubscription/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.androidsubscription;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submitBtn = findViewById(R.id.submit);
+        Button clearBtn = findViewById(R.id.clear);
+        final TextView status = findViewById(R.id.status);
+        final EditText username = findViewById(R.id.username);
+        final EditText email = findViewById(R.id.email);
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = getString(R.string.status);
+                status.setText(
+                        String.format(
+                                message,
+                                username.getText().toString(),
+                                email.getText().toString()
+                        )
+                );
+            }
+        });
+
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                username.setText("");
+                email.setText("");
+                status.setText("");
+            }
+        });
     }
 }
